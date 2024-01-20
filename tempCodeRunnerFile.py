699ca8a24f040e8a476c5aaf7c216d2583c13deb
definite-d <38317208+definite-d@users.noreply.github.com>
2024-01-20 19:54:16 +0100

    Cleaning up.

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-Tkinter Calculator Implementation with Python
-Copyright © 2024 Divine Afam-Ifediogor
-
-This code contains my implementation and explanations of a basic Calculator App in Tkinter, 
-a GUI toolkit for Python.
